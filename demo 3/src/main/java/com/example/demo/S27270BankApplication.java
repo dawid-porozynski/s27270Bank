355fbc8654d6a31da5ffe1238c8b4fe5a4ebf354
dawid-porozynski <pora13234@gmail.com>
2024-01-27 15:05:30 +0100

    test

---
+++ demo 3/src/main/java/com/example/demo/S27270BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27270BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S27270BankApplication.class, args);
+	}
+
+}
