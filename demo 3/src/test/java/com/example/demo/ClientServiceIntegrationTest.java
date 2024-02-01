e754b68a79f1d7737f4d4b1e7e24f396b169999a
dawid-porozynski <pora13234@gmail.com>
2024-02-01 14:31:55 +0100

    test

--- demo 3/src/test/java/com/example/demo/ClientServiceIntegrationTest.java
+++ demo 3/src/test/java/com/example/demo/ClientServiceIntegrationTest.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -30,6 +30,22 @@ class ClientServiceIntegrationTest {
         Transfer transfer = clientService.makeTransfer("xxx", 25);
         // THEN
         assertThat(clientStorage.getCustomerByName("xxx").get().getBalance()).isEqualTo(475);
+        assertThat(transfer.getStatusType()).isEqualTo(StatusType.ACCEPTED);
+    }
+
+    void shouldAddClient(){
+        clientService.clientSignUp(250, "xxx");
+        // THEN
+        assertThat(clientStorage.getCustomerByName("xxx").isPresent()).isTrue();
+    }
+    @Test
+    void shouldDepositMoney() {
+        // GIVEN
+        when(clientStorage.getCustomerByName("xxx")).thenReturn(Optional.of(new Client("xxx",500 )));
+        // WHEN
+        Transfer transfer = clientService.depositMoney("xxx", 25);
+        // THEN
+        assertThat(clientStorage.getCustomerByName("xxx").get().getBalance()).isEqualTo(525);
         assertThat(transfer.getStatusType()).isEqualTo(StatusType.ACCEPTED);
     }
 }
